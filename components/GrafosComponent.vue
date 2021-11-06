0b8d2948bd5e06da393ddd15823b172d0722f478
Seba2197 <scoronado@utem.cl>
2021-11-05 22:11:11 -0300

    ADD: Botones

--- components/GrafosComponent.vue
+++ components/GrafosComponent.vue
@@ -1,5 +1,20 @@
 <template>
+<div>
+ <h1 class="text-center textocolor fredoka mt-5 pt-4">Crear Autómata</h1>
+        <div class="row justify-content-center" >
+            <div class="grafo1 col-md-3 mx-4 card cardaux">
+                <div class="container-fluid mr-4">
+                    <div class="row justify-content-center ml-1">
 
+                            <div class="col-md-6 my-4"><button class="btn btn-success info textocolor">Crear AFD</button></div>
+                            <div class="col-md-6 my-4"><button class="btn btn-success info textocolor">Crear AP's</button></div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+</div>
 </template>
 
 <script>
