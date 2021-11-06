c4890bd31a24f556ca6cafb5e5260381b60a72de
WolFir3z <mzapata@utem.cl>
2021-11-06 20:41:38 -0300

    ADD: Botones

--- components/GrafosComponent.vue
+++ components/GrafosComponent.vue
@@ -1,5 +1,78 @@
 <template>
+<div>
+ <h1 class="text-center textocolor fredoka mt-5 pt-4">Crear Autómata</h1>
+        <div class="row justify-content-center" >
+            <div class="grafo1 col-md-3 mx-4 card cardaux">
+                <div class="container-fluid mr-4">
+                    <div class="row justify-content-center ml-1">
+                        
+                            <div class="col-md-6 my-4"><button class="btn btn-success info textocolor">Crear AFD</button></div>
+                            <div class="col-md-6 my-4"><button class="btn btn-success info textocolor">Crear AP's</button></div>
+                        
+                    </div>
 
+                </div>
+
+                <div class="container-fluid py-4 mr-4">
+                    <h3 class="mt-2">Autómata Finito Determinista (AFD)</h3>
+                    <hr>
+                    <div class="row text-center my-4">
+                        <div class="col-md-4">
+                            <button class="btn btn-sm btn-primary">Añadir Estado</button>
+                        </div>
+                        <div class="col-md-4">
+                            <button class="btn btn-sm btn-success">Añadir Transición</button>
+                        </div>
+                        <div class="col-md-4">
+                            <button class="btn btn-sm btn-success">Modificar Estado Final</button>
+                        </div>
+                    </div>
+                    <div class="row text-center">
+                        <div class="col-md-4">
+                            <button class="btn btn-sm btn-danger">Eliminar Autómata</button>
+                        </div>
+                        <div class="col-md-4"></div>
+                        <div class="col-md-4">
+                            <button class="btn btn-sm btn-warning"> Volver al Inicio</button>
+                        </div>                       
+                    </div>
+                    <button class="btn btn-success btn-sm">Volver</button>
+                    <div class="my-4">
+                        <div>
+                            <form>
+                                <div class="form-group">
+                                    <label for="id">Ingrese el id: </label>
+                                    <input type="number" min="0" class="form-control">
+                                </div>
+                                <div class="text-center">
+                                    <button class="btn btn-success btn-sm" type="submit">Agregar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div class="my-3">
+                        <form >
+                            <div class="form-group">
+                                <label>Ingrese el id del estado inicial de la transición:</label>
+                                <input type="number" min="0" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Ingrese el id del estado final de la transición:</label>
+                                <input type="number" min="0" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Ingrese carácter de la transición: </label>
+                                <input type="text" minlength="1" class="form-control">
+                            </div>
+
+                            <button class="btn btn-success btn-sm" type="submit">Agregar</button>
+                        </form>
+                    </div> 
+                </div>
+            </div>
+        </div>
+</div>
 </template>
 
 <script>
