6c888d53973374384142175056f6ef30420c13bd
Zrk00 <mmoreno@utem.cl>
2021-11-06 21:18:13 -0300

    ADD: Campos de ingreso

--- components/GrafosComponent.vue
+++ components/GrafosComponent.vue
@@ -71,7 +71,125 @@
             </div>
         </div>
     </div>
-</div>
+     <div class="container-fluid py-4 mr-4">
+                    <h3 class="mt-2">Autómatas de Pila (AP)</h3>
+                    <div class="row text-center my-4">
+                        <div class="col-md-6">
+                            <button class="btn btn-sm btn-success">Crear AP 1</button>
+                        </div>
+                        <div class="col-md-6">
+                            <button class="btn btn-sm btn-success">Crear AP 2</button>
+                        </div>
+                    </div>
+                    <div class="row my-2">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-4 text-center">
+                            <button class="btn btn-sm btn-warning">Volver al Inicio</button>
+                        </div>
+                    </div>
+
+                    <div class="row text-center my-4">
+                        <div class="col-md-4">
+                            <button class="btn btn-sm btn-success">Añadir Estado</button>
+                        </div>
+                        <div class="col-md-4">
+                            <button class="btn btn-sm btn-success">Añadir Transición</button>
+                        </div>
+                        <div class="col-md-4">
+                            <button class="btn btn-sm btn-success">Modificar Estado Final</button>
+                        </div>
+                    </div>
+
+                    <button class="btn btn-success btn-sm mt-3">Volver</button>
+
+                    <div class="row text-center">
+                        <div class="col-md-4">
+                            <button class="btn btn-sm btn-danger">Eliminar Autómata</button>
+                        </div>
+                        <div class="col-md-4"></div>
+                        <div class="col-md-4">
+                            <button class="btn btn-sm btn-warning"> Volver</button>
+                        </div>                       
+                    </div>
+
+                    <div class="my-4">
+                        <div>
+                            <form >
+                                <div class="form-group">
+                                    <label for="id">Ingrese el id: </label>
+                                    <input type="number" min="0" class="form-control" >
+                                </div>
+                                <div class="text-center">
+                                    <button class="btn btn-success btn-sm" type="submit">Agregar</button>
+                                </div>
+                            </form>
+
+                            <form >
+                                <div class="form-group">
+                                    <label for="id">Ingrese el id: </label>
+                                    <input type="number" min="0" class="form-control">
+                                </div>
+                                <div class="text-center">
+                                    <button class="btn btn-success btn-sm" type="submit">Agregar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div class="my-3">
+                        <form >
+                            <div class="form-group">
+                                <label>Ingrese el id del estado inicial de la transición:</label>
+                                <input type="number" min="0" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Ingrese el id del estado final de la transición:</label>
+                                <input type="number" min="0" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Ingrese carácter de la transición: </label>
+                                <input type="text" minlength="1" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Push(): </label>
+                                <input type="text" minlength="1" class="form-control" placeholder="dejar vacío para epsilon">
+                            </div>
+                            <div class="form-group">
+                                <label>Pop(): </label>
+                                <input type="text" minlength="1" class="form-control" placeholder="dejar vacío para epsilon">
+                            </div>
+
+                            <button class="btn btn-success btn-sm" type="submit">Agregar</button>
+                        </form>
+
+                        <form >
+                            <div class="form-group">
+                                <label>Ingrese el id del estado inicial de la transición:</label>
+                                <input type="number" min="0" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Ingrese el id del estado final de la transición:</label>
+                                <input type="number" min="0" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Ingrese carácter de la transición: </label>
+                                <input type="text" minlength="1" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Push(): </label>
+                                <input type="text" minlength="1" class="form-control" placeholder="dejar vacío para epsilon">
+                            </div>
+                            <div class="form-group">
+                                <label>Pop(): </label>
+                                <input type="text" minlength="1" class="form-control" placeholder="dejar vacío para epsilon">
+                            </div>
+
+                            <button class="btn btn-success btn-sm" type="submit">Agregar</button>
+                        </form>
+                    </div> 
+                </div>
+                
+            </div>
 </template>
 
 <script>
